249719d1a0d04014696907f42e4804a5a2924ab4
floating-cat <monsoon.cl@gmail.com>
2025-01-10 20:01:21 +0800

    Initial commit

---
+++ config/key-labels-eyelash-sofle-64.h
@@ -0,0 +1,73 @@
+// forked from https://github.com/urob/zmk-helpers/blob/b425009a3e13877ee413ae9242fc13b9aff8e1ca/include/zmk-helpers/key-labels/sofle.h
+#pragma once
+
+#define LN0  5  // left-number row
+#define LN1  4
+#define LN2  3
+#define LN3  2
+#define LN4  1
+#define LN5  0
+
+#define RN0  7  // right-number row
+#define RN1  8
+#define RN2  9
+#define RN3 10
+#define RN4 11
+#define RN5 12
+
+#define LT0 18  // left-top row
+#define LT1 17
+#define LT2 16
+#define LT3 15
+#define LT4 14
+#define LT5 13
+
+#define RT0 20  // right-top row
+#define RT1 21
+#define RT2 22
+#define RT3 23
+#define RT4 24
+#define RT5 25
+
+#define LM0 31  // left-middle row
+#define LM1 30
+#define LM2 29
+#define LM3 28
+#define LM4 27
+#define LM5 26
+
+#define RM0 33  // right-middle row
+#define RM1 34
+#define RM2 35
+#define RM3 36
+#define RM4 37
+#define RM5 38
+
+#define LB0 44  // left-bottom row
+#define LB1 43
+#define LB2 42
+#define LB3 41
+#define LB4 40
+#define LB5 39
+
+#define LEC 52  // left encoder key
+#define REC 58  // right encoder key
+
+#define RB0 46  // right-bottom row
+#define RB1 47
+#define RB2 48
+#define RB3 49
+#define RB4 50
+#define RB5 51
+
+#define LH0 57  // left thumb keys
+#define LH1 56
+#define LH2 55
+#define LH3 54
+#define LH4 53
+
+#define RH0 59  // right thumb keys
+#define RH1 60
+#define RH2 61
+#define RH3 62
+#define RH4 63
